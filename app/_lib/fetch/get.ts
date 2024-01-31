d3b4eebf44e344da6a472ac4fa35d942aa6de273
charlesoller <charlesrello@gmail.com>
2024-01-31 12:14:18 -0500

    cleaning up intial page

--- app/_lib/fetch/get.ts
+++ app/_lib/fetch/get.ts
@@ -9,7 +9,6 @@ export const getTwurs = async() => {
 }
 
 export const getTwur = async(id: string) => {
-    console.log("THIS IS THE ID", id)
     const res = await fetch(`${URL}/api/twurs/${id}`, {
         method: "GET"
     }).then(res => res.json())
